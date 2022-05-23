64404477593191c411343fbeb92ceeba7948b87d
graffiti149 <aditi_ug@cse.nits.ac.in>
2022-05-23 22:01:01 +0530

    made changes in the app.js file

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
-import Map from './components/Map'
-import { useState, useEffect } from 'react';
+import Map from "./components/Map";
+import { useState, useEffect } from "react";
 
 function App() {
-  const[eventData, setEventData] = useState([])
+  const [eventData, setEventData] = useState([]);
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    const fetchEvents = async() => {
-      setLoading(true)
-      const res = await fetch('https://eonet.gsfc.nasa.gov/api/v2.1/events');
-      const {events} = await res.json();
+    const fetchEvents = async () => {
+      setLoading(true);
+      const res = await fetch(
+        "https://eonet.sci.gsfc.nasa.gov/api/v2.1/events"
+      );
+      const { events } = await res.json();
+      console.log(events, "events");
 
-      setEventData(events)
-      setLoading(false)
-    }
+      setEventData(events);
+      setLoading(false);
+    };
 
-    fetchEvents()
-  }, [])
-
+    fetchEvents();
+  }, []);
 
   return (
-    <div>
-    { !loading ? <Map eventData = {eventData} />  : <h1> Loading</h1>}
-    </div>
+    // <div>{!loading ? <Map eventData={eventData} /> : <h1> Loading</h1>}</div>
+    <div>{eventData ? <Map eventData={eventData} /> : <div>Loading</div>}</div>
   );
 }
 
